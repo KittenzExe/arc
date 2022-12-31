155ea9925491fbc42678e7b671d8f1d7815f2bb9
KittenzExe <67358250+KittenzExe@users.noreply.github.com>
2023-01-01 04:45:37 +0800

    menu switching added

--- arc.py
+++ arc.py
@@ -36,12 +36,6 @@ if gameLoop == 1:
     homeBG = pygame.image.load("songs/"+ randBG+"/background.png")
     homeBG = pygame.transform.scale(homeBG, (wx, wy))
 
-
-    uNameButton = pygame.Rect(63, 5, 200, 24)
-    pWordButton = pygame.Rect(63, 34, 200, 24)
-    dSubmitButton = pygame.Rect(5, 5, 53, 53)
-    logoButton = pygame.Rect(((wx/2)-250), ((wy/2)-250), 500, 500)
-
     font = pygame.font.SysFont("arialrounded", 20)#, bold = pygame.font.Font.bold
 
 fpsON = 1
@@ -74,6 +68,8 @@ tInputP = False
 dSubmit = False
 loginShow = True
 
+menu = 0
+
 uName = ''
 uScore = ''
 uPP = ''
@@ -86,198 +82,203 @@ logONU = False
 logONP = False
 
 print(randBG)
-window.blit(homeBG, (0,0))
-
+flashBG = 1
 
 while True:
     mainClock.tick(999)
+
+    if flashBG == 1:
+        window.blit(homeBG, (0,0))
+        time.sleep(0.0000001)
+        flashBG = 0
 
     window.blit(logoH, (((wx/2)-250), ((wy/2)-250)))
     bar = (124, 124, 124)
     bar1 = pygame.Rect(0, 0, wx, 70)
     pygame.draw.rect(window, bar, bar1)
 
-    #fps update
-    if fpsON == 1:
-        newFPS = int(mainClock.get_fps())
-        rawTime = int(mainClock.get_rawtime())
-        fpsR = (255,0,0)
-        fpsO = (255,165,0)
-        fpsY = (255,255,0)
-        fpsL = (165,255,0)
-        fpsG = (0,255,0)
-        fpsBox = pygame.Rect((wx - 85) , (wy - 30), 100, 25)
-        rawtimeBox = pygame.Rect((wx - 70) , (wy - 56), 100, 23)
-        if newFPS >= 15:
-            pygame.draw.rect(window, fpsR, fpsBox)
-        if newFPS >= 30:
-            pygame.draw.rect(window, fpsO, fpsBox)
-        if newFPS >= 60:
-            pygame.draw.rect(window, fpsY, fpsBox)
-        if newFPS >= 120:
-            pygame.draw.rect(window, fpsL, fpsBox)
-        if newFPS >= 180:
-            pygame.draw.rect(window, fpsG, fpsBox)
-        if rawTime <= 5:
-            pygame.draw.rect(window, fpsR, rawtimeBox)
-        if rawTime == 4:
-            pygame.draw.rect(window, fpsO, rawtimeBox)
-        if rawTime == 3:
-            pygame.draw.rect(window, fpsY, rawtimeBox)
-        if rawTime == 2:
-            pygame.draw.rect(window, fpsL, rawtimeBox)
-        if rawTime == 1:
-            pygame.draw.rect(window, fpsG, rawtimeBox)
-        window.blit(update_fps(), ((wx - 80),(wy - 31)))
-        window.blit(update_rawtime(), ((wx - 69),(wy - 56)))
-    
-    home = True
-    menu = False
-
-    if home == True:
-        for event in pygame.event.get():
-            if event.type == QUIT:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
                 pygame.quit()
                 sys.exit()
-            if event.type == KEYDOWN:
-                if event.key == K_ESCAPE:
-                    pygame.quit()
-                    sys.exit()
+        if event.type == KEYDOWN:
+            if event.key == K_BACKSPACE:
+                login = 1
+                menu = 0
+                flashBG = 1
 
-            if login == 1:
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                        mouse_pos = event.pos  # gets mouse position
+    if login == 1:
 
-                        if uNameButton.collidepoint(mouse_pos):
-                            # prints current location of mouse
-                            print('button was pressed at {0}'.format(mouse_pos))
-                            tInputU = True
-                            tInputP = False
-                            dSubmit = False
-                            print(tInputU)
-                            print(tInputP)
-                            print(dSubmit)
-                            uT = False
+        if fpsON == 1:
+            newFPS = int(mainClock.get_fps())
+            rawTime = int(mainClock.get_rawtime())
+            fpsR = (255,0,0)
+            fpsO = (255,165,0)
+            fpsY = (255,255,0)
+            fpsL = (165,255,0)
+            fpsG = (0,255,0)
+            fpsBox = pygame.Rect((wx - 85) , (wy - 30), 100, 25)
+            rawtimeBox = pygame.Rect((wx - 70) , (wy - 56), 100, 23)
+            if newFPS >= 15:
+                pygame.draw.rect(window, fpsR, fpsBox)
+            if newFPS >= 30:
+                pygame.draw.rect(window, fpsO, fpsBox)
+            if newFPS >= 60:
+                pygame.draw.rect(window, fpsY, fpsBox)
+            if newFPS >= 120:
+                pygame.draw.rect(window, fpsL, fpsBox)
+            if newFPS >= 180:
+                pygame.draw.rect(window, fpsG, fpsBox)
+            if rawTime <= 5:
+                pygame.draw.rect(window, fpsR, rawtimeBox)
+            if rawTime == 4:
+                pygame.draw.rect(window, fpsO, rawtimeBox)
+            if rawTime == 3:
+                pygame.draw.rect(window, fpsY, rawtimeBox)
+            if rawTime == 2:
+                pygame.draw.rect(window, fpsL, rawtimeBox)
+            if rawTime == 1:
+                pygame.draw.rect(window, fpsG, rawtimeBox)
+            window.blit(update_fps(), ((wx - 80),(wy - 31)))
+            window.blit(update_rawtime(), ((wx - 69),(wy - 56)))
 
-                        if pWordButton.collidepoint(mouse_pos):
-                            # prints current location of mouse
-                            print('button2 was pressed at {0}'.format(mouse_pos))
-                            tInputU = False
-                            tInputP = True
-                            dSubmit = False
-                            print(tInputU)
-                            print(tInputP)
-                            print(dSubmit)
-                            pT = False
-                        
-                        if dSubmitButton.collidepoint(mouse_pos):
-                            # prints current location of mouse
-                            print('button3 was pressed at {0}'.format(mouse_pos))
-                            tInputU = False
-                            tInputP = False
-                            dSubmit = True
-                            print(tInputU)
-                            print(tInputP)
-                            print(dSubmit)
-                        
-                        if logoButton.collidepoint(mouse_pos):
-                            # prints current location of mouse
-                            print('logo was pressed at {0}'.format(mouse_pos))
-                            print("logo clicked!")
-                            menu = True
-                            time.sleep(0.1)
-                            home = False
+        uNameButton = pygame.Rect(63, 5, 200, 24)
+        pWordButton = pygame.Rect(63, 34, 200, 24)
+        dSubmitButton = pygame.Rect(5, 5, 53, 53)
+        logoButton = pygame.Rect(((wx/2)-250), ((wy/2)-250), 500, 500)
 
-                if tInputU == True:
-                    if event.type == pygame.KEYDOWN:
-                        logOnU = True
-                        if event.key == pygame.K_BACKSPACE:
-                            text_value_U = text_value_U[:-1]
-                            text_U = font.render(text_value_U, True, (0, 0, 0))
-                        if event.key == pygame.K_RETURN:
-                            print(text_value_U)
-                    if event.type == pygame.TEXTINPUT:
-                        text_value_U += event.text
-                        text_U = font.render(text_value_U, True, (0, 0, 0))
+        if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = event.pos  # gets mouse position
 
-                if tInputP == True:
-                    if event.type == pygame.KEYDOWN:
-                        logOnP = True
-                        if event.key == pygame.K_BACKSPACE:
-                            text_value_P = text_value_P[:-1]
-                            text_P = font.render(text_value_P, True, (0, 0, 0))
-                        if event.key == pygame.K_RETURN:
-                            print(text_value_P)
-                    if event.type == pygame.TEXTINPUT:
-                        text_value_P += event.text
-                        text_P = font.render(text_value_P, True, (0, 0, 0))
+                if uNameButton.collidepoint(mouse_pos):
+                    # prints current location of mouse
+                    print('button was pressed at {0}'.format(mouse_pos))
+                    tInputU = True
+                    tInputP = False
+                    dSubmit = False
+                    print(tInputU)
+                    print(tInputP)
+                    print(dSubmit)
+                    uT = False
+
+                if pWordButton.collidepoint(mouse_pos):
+                    # prints current location of mouse
+                    print('button2 was pressed at {0}'.format(mouse_pos))
+                    tInputU = False
+                    tInputP = True
+                    dSubmit = False
+                    print(tInputU)
+                    print(tInputP)
+                    print(dSubmit)
+                    pT = False
                 
-                if dSubmit == True:
-                    #Username Handler
-                    doc_ref1 = db.collection(u'spindal users').document(u''+text_value_U)
-                    doc1 = doc_ref1.get()
-                    entered += 1
-                    print(entered)
-                    if doc1.exists:
-                        doc_ref1.set({
-                            u'3-uName': text_value_U,
-                            u'2-curveConnected': 0,
-                            u'1-sessionID': res,
-                        }, merge=True)
-                    else:
-                        print(u'No such document!')
-                    
-                    #Password Handler
-                    message_bytes_P = text_value_P.encode('ascii')
-                    base64_bytes = base64.b64encode(message_bytes_P)
-                    base64_message_p = base64_bytes.decode('ascii')
-                    doc_ref1 = db.collection(u'spindal users').document(u''+text_value_U)
-                    doc1 = doc_ref1.get()
-                    entered += 1
-                    print(entered)
-                    if doc1.exists:
-                        doc_ref1.set({
-                            u'4-pWord': base64_message_p,
-                        }, merge=True)
-                    else:
-                        print(u'No such document!')
-                    
-                    #Verif. Handler
-                    print("Data Sent! Awaiting Verification of client...")
-                    datab = firestore.client()
-                    docu = text_value_U
-                    doc_ref2 = datab.collection(u'spindal users').document(u''+text_value_U)
-                    doc2 = doc_ref2.get()
-                    #Weird delay between sending and recieving...
-                    if doc2.exists:
-                        print(f'Document data: {doc2.to_dict()}')
-                        sIDCALL = (f"{doc2.to_dict()}")
-                        verif = str(doc2.to_dict()['1-sessionID'])
-                        print(verif)
-                        if verif == res:
-                            print("verified!")
-                            print("have fun playing!")
-                            loginShow = False
-                            login = 2
-                            doc_ref1.set({
-                            u'2-curveConnected': 1
-                        }, merge=True)
-                        else:
-                            print("verification failed. please restart")
-                        
-                        uName = str(doc2.to_dict()['3-uName'])
-                        uScore = str(doc2.to_dict()['5-score'])
-                        uPP = str(doc2.to_dict()['6-pp'])
-                        print(uName+" , "+uScore+" , "+uPP)
+                if dSubmitButton.collidepoint(mouse_pos):
+                    # prints current location of mouse
+                    print('button3 was pressed at {0}'.format(mouse_pos))
+                    tInputU = False
+                    tInputP = False
+                    dSubmit = True
+                    print(tInputU)
+                    print(tInputP)
+                    print(dSubmit)
+                
+                if logoButton.collidepoint(mouse_pos):
+                    # prints current location of mouse
+                    print('logo was pressed at {0}'.format(mouse_pos))
+                    print("logo clicked!")
+                    menu = 1
+                    login = 0
 
-                        #Finish submit loop
-                        dSubmit = False
-                    else:
-                        print(u'No such document!')
+        if tInputU == True:
+            if event.type == pygame.KEYDOWN:
+                logOnU = True
+                if event.key == pygame.K_BACKSPACE:
+                    text_value_U = text_value_U[:-1]
+                    text_U = font.render(text_value_U, True, (0, 0, 0))
+                if event.key == pygame.K_RETURN:
+                    print(text_value_U)
+            if event.type == pygame.TEXTINPUT:
+                text_value_U += event.text
+                text_U = font.render(text_value_U, True, (0, 0, 0))
 
-    if menu == True:
-        print("menu")
-        window.fill(10,10,10)
+        if tInputP == True:
+            if event.type == pygame.KEYDOWN:
+                logOnP = True
+                if event.key == pygame.K_BACKSPACE:
+                    text_value_P = text_value_P[:-1]
+                    text_P = font.render(text_value_P, True, (0, 0, 0))
+                if event.key == pygame.K_RETURN:
+                    print(text_value_P)
+            if event.type == pygame.TEXTINPUT:
+                text_value_P += event.text
+                text_P = font.render(text_value_P, True, (0, 0, 0))
+        
+        if dSubmit == True:
+            #Username Handler
+            doc_ref1 = db.collection(u'spindal users').document(u''+text_value_U)
+            doc1 = doc_ref1.get()
+            entered += 1
+            print(entered)
+            if doc1.exists:
+                doc_ref1.set({
+                    u'3-uName': text_value_U,
+                    u'2-curveConnected': 0,
+                    u'1-sessionID': res,
+                }, merge=True)
+            else:
+                print(u'No such document!')
+            
+            #Password Handler
+            message_bytes_P = text_value_P.encode('ascii')
+            base64_bytes = base64.b64encode(message_bytes_P)
+            base64_message_p = base64_bytes.decode('ascii')
+            doc_ref1 = db.collection(u'spindal users').document(u''+text_value_U)
+            doc1 = doc_ref1.get()
+            entered += 1
+            print(entered)
+            if doc1.exists:
+                doc_ref1.set({
+                    u'4-pWord': base64_message_p,
+                }, merge=True)
+            else:
+                print(u'No such document!')
+            
+            #Verif. Handler
+            print("Data Sent! Awaiting Verification of client...")
+            datab = firestore.client()
+            docu = text_value_U
+            doc_ref2 = datab.collection(u'spindal users').document(u''+text_value_U)
+            doc2 = doc_ref2.get()
+            #Weird delay between sending and recieving...
+            if doc2.exists:
+                print(f'Document data: {doc2.to_dict()}')
+                sIDCALL = (f"{doc2.to_dict()}")
+                verif = str(doc2.to_dict()['1-sessionID'])
+                print(verif)
+                if verif == res:
+                    print("verified!")
+                    print("have fun playing!")
+                    loginShow = False
+                    login = 2
+                    doc_ref1.set({
+                    u'2-curveConnected': 1
+                }, merge=True)
+                else:
+                    print("verification failed. please restart")
+                
+                uName = str(doc2.to_dict()['3-uName'])
+                uScore = str(doc2.to_dict()['5-score'])
+                uPP = str(doc2.to_dict()['6-pp'])
+                print(uName+" , "+uScore+" , "+uPP)
+
+                #Finish submit loop
+                dSubmit = False
+            else:
+                print(u'No such document!')
 
     if loginShow == True:
         pygame.draw.rect(window, [240, 240, 240], uNameButton)
@@ -300,5 +301,41 @@ while True:
         window.blit(uNamePrint, (5, 5))
         window.blit(uScorePrint, (5, 25))
         window.blit(uPPPrint, (5, 45))
+
+    if menu == 1:
+        window.fill((240,240,240))
+
+        if fpsON == 1:
+            newFPS = int(mainClock.get_fps())
+            rawTime = int(mainClock.get_rawtime())
+            fpsR = (255,0,0)
+            fpsO = (255,165,0)
+            fpsY = (255,255,0)
+            fpsL = (165,255,0)
+            fpsG = (0,255,0)
+            fpsBox = pygame.Rect((wx - 85) , (wy - 30), 100, 25)
+            rawtimeBox = pygame.Rect((wx - 70) , (wy - 56), 100, 23)
+            if newFPS >= 15:
+                pygame.draw.rect(window, fpsR, fpsBox)
+            if newFPS >= 30:
+                pygame.draw.rect(window, fpsO, fpsBox)
+            if newFPS >= 60:
+                pygame.draw.rect(window, fpsY, fpsBox)
+            if newFPS >= 120:
+                pygame.draw.rect(window, fpsL, fpsBox)
+            if newFPS >= 180:
+                pygame.draw.rect(window, fpsG, fpsBox)
+            if rawTime <= 5:
+                pygame.draw.rect(window, fpsR, rawtimeBox)
+            if rawTime == 4:
+                pygame.draw.rect(window, fpsO, rawtimeBox)
+            if rawTime == 3:
+                pygame.draw.rect(window, fpsY, rawtimeBox)
+            if rawTime == 2:
+                pygame.draw.rect(window, fpsL, rawtimeBox)
+            if rawTime == 1:
+                pygame.draw.rect(window, fpsG, rawtimeBox)
+            window.blit(update_fps(), ((wx - 80),(wy - 31)))
+            window.blit(update_rawtime(), ((wx - 69),(wy - 56)))
 
     pygame.display.flip()
